62906690ed1bcb54120565738f6274f640918e8b
Enrico Murru <enrico.murru@gmail.com>
2016-12-18 13:22:12 +0100

    polished code

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var port = /*(process.argv.length > 2) ? process.argv[2] : */(process.env.PORT || 3000);
-
+var port = process.env.PORT || 3000;
 function setHeaders(res, path) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 }
-
 connect().use(serveStatic(__dirname+"/root/",{
 	"setHeaders": setHeaders,
 })).listen(parseInt(port));
