4790ad6efee260816aacedf736810ffb77a8a903
Enrico Murru <enrico.murru@gmail.com>
2016-12-18 13:13:48 +0100

    Added headers manipulation

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var port = (process.argv.length > 2) ? process.argv[2] : 3000;
-connect().use(serveStatic(__dirname+"/root/")).listen(parseInt(port));
+var port = /*(process.argv.length > 2) ? process.argv[2] : */(process.env.PORT || 3000);
+
+function setHeaders(res, path) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+}
+
+connect().use(serveStatic(__dirname+"/root/",{
+	"setHeaders": setHeaders,
+})).listen(parseInt(port));
 console.log("Simple static web server running on port "+port);
