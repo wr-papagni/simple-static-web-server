0860842413901eeb0eb1a51c31dbe1e77c017242
pierpes <pierpes@users.noreply.github.com>
2018-02-14 12:48:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var serveStatic = require('serve-static');
 var port = process.env.PORT || 3000;
 function setHeaders(res, path) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 }
 connect().use(serveStatic(__dirname+"/root/",{
 	"setHeaders": setHeaders,
